5d89ce767cc0217b664feab89b194d43130e9e82
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-10-30 19:32:04 +0100

    Edit divide-conquer tests to use chai

--- src/divide-conquer/find-num-in-sorted-arr.spec.js
+++ src/divide-conquer/find-num-in-sorted-arr.spec.js
@@ -1,32 +1,32 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const findNumInSortedArr = require('./find-num-in-sorted-arr');
 
-describe('Index of Value Sorted', function() {
+describe('Index of Value Sorted', function () {
   it('finds number in position 0', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 1), 0);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 1)).to.equal(0);
   });
 
   it('finds number in position 1', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 2), 1);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 2)).to.equal(1);
   });
 
   it('finds number in position 3', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 4), 3);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 4)).to.equal(3);
   });
 
   it('finds number in position 5', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 6), 5);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 6)).to.equal(5);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 11), -1);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 11)).to.equal(-1);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(findNumInSortedArr([1, 2, 3, 4, 5, 6], 0), -1);
+    expect(findNumInSortedArr([1, 2, 3, 4, 5, 6], 0)).to.equal(-1);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(findNumInSortedArr([], 9), -1);
+    expect(findNumInSortedArr([], 9)).to.equal(-1);
   });
 });

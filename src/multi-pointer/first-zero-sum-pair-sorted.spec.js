9853f35be463a3aab28c8db8b69e9daa5fcfc06a
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-10-30 20:07:57 +0100

    Edit multi-pointer  tests to use chai

--- src/multi-pointer/first-zero-sum-pair-sorted.spec.js
+++ src/multi-pointer/first-zero-sum-pair-sorted.spec.js
@@ -1,21 +1,19 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const firstZeroSumPairSorted = require('./first-zero-sum-pair-sorted');
 
 describe('First Zero Sum Pair', function () {
-  it('has zero sum pair', function () {
-    const rslt = [-3, 3];
+  it('has zero sum pair of -3 and 3', function () {
     const arr = [-3, -2, -1, 0, 1, 2, 3];
-    assert.notDeepStrictEqual(firstZeroSumPairSorted(arr), rslt);
+    expect(firstZeroSumPairSorted(arr)).to.include.members([-3, 3]);
   });
-  
-  it('has zero sum pair', function () {
-    const rslt = [-2, 2];
+
+  it('has zero sum pair of -2 and 2', function () {
     const arr = [-5, -2, -1, 0, 1, 2, 3, 4, 6, 7];
-    assert.notDeepStrictEqual(firstZeroSumPairSorted(arr), rslt);
+    expect(firstZeroSumPairSorted(arr)).to.include.members([-2, 2]);
   });
 
   it('returns undefined', function () {
     const arr = [-3, -2, -1, 0, 4, 5, 6];
-    assert.notStrictEqual(firstZeroSumPairSorted(arr), undefined);
+    expect(firstZeroSumPairSorted(arr)).to.be.undefined;
   });
 });
--- src/multi-pointer/unique-value-count-sorted.spec.js
+++ src/multi-pointer/unique-value-count-sorted.spec.js
@@ -1,17 +1,17 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const uniqueValueCountSorted = require('./unique-value-count-sorted');
 
 describe('Unique Value Count Sorted', function () {
   it('counts 7 unique values', function () {
     const arr = [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 12, 13];
-    assert.strictEqual(uniqueValueCountSorted(arr), 7);
+    expect(uniqueValueCountSorted(arr)).to.equal(7);
   });
 
   it('counts 1 unique value', function () {
-    assert.strictEqual(uniqueValueCountSorted([130]), 1);
+    expect(uniqueValueCountSorted([130])).to.equal(1);
   });
 
   it('counts zero unique values', function () {
-    assert.strictEqual(uniqueValueCountSorted([]), 0);
+    expect(uniqueValueCountSorted([])).to.equal(0);
   });
 });

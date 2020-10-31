69b52e1e7b284b0259d626bc412fab7e60f5b4b9
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-10-31 22:04:56 +0100

    Edit search tests to use chai

--- src/search/binary-search.spec.js
+++ src/search/binary-search.spec.js
@@ -1,32 +1,32 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const binarySearch = require('./binary-search');
 
-describe('Index of Value Sorted', function() {
+describe('Index of Value Sorted', function () {
   it('finds number in position 0', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 1), 0);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 1)).to.equal(0);
   });
 
   it('finds number in position 1', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 2), 1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 2)).to.equal(1);
   });
 
   it('finds number in position 3', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 4), 3);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 4)).to.equal(3);
   });
 
   it('finds number in position 5', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 6), 5);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 6)).to.equal(5);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 11), -1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 11)).to.equal(-1);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(binarySearch([1, 2, 3, 4, 5, 6], 0), -1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 0)).to.equal(-1);
   });
 
   it('returns -1', function () {
-    assert.strictEqual(binarySearch([], 9), -1);
+    expect(binarySearch([], 9)).to.equal(-1);
   });
 });
--- src/search/substring.spec.js
+++ src/search/substring.spec.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const substring = require('./substring');
 
 describe('Substring', function () {
-  it('finds at 3', function () { assert.strictEqual(substring('abcdefghij', 'def'), 3); });
-  it('finds at 0', function () { assert.strictEqual(substring('chip off the old block', 'chip'), 0); });
-  it('finds at 20', function () { assert.strictEqual(substring('what eyes have not seen', 'een'), 20); });
-  it('finds at 12', function () { assert.strictEqual(substring('what is the way forward', 'way'), 12);});
-  it('does not find', function () { assert.strictEqual(substring('the hills are alive', 'maria'), -1); });
-  it('finds at 1', function () { assert.strictEqual(substring('eel', 'el'), 1); });
+  it('finds at 3', function () { expect(substring('abcdefghij', 'def')).to.equal(3); });
+  it('finds at 0', function () { expect(substring('chip off the old block', 'chip')).to.equal(0); });
+  it('finds at 20', function () { expect(substring('what eyes have not seen', 'een')).to.equal(20); });
+  it('finds at 12', function () { expect(substring('what is the way forward', 'way')).to.equal(12); });
+  it('does not find', function () { expect(substring('the hills are alive', 'maria')).to.equal(-1); });
+  it('finds at 1', function () { expect(substring('eel', 'el')).to.equal(1); });
 });

4981ca33da84dec083d859c321561252050bfb0f
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-11-01 23:02:36 +0100

    Edit sorting tests to use chai

--- src/sorting/comparison/merge-sort.spec.js
+++ src/sorting/comparison/merge-sort.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const mergeSort = require('./merge-sort');
 
-describe.only('Bubble Sort', function () {
+describe('Bubble Sort', function () {
   it('sorts array', function () { expect(mergeSort([2, 4, 6, 1, 3, 5])).to.contain.members([1, 2, 3, 4, 5, 6]); });
   it('sorts array', function () { expect(mergeSort([2, 1, 3, 4, 5, 6])).to.contain.members([1, 2, 3, 4, 5, 6]); });
   it('sorts array', function () { expect(mergeSort([2, 3, 1, 4, 5, 6])).to.contain.members([1, 2, 3, 4, 5, 6]); });

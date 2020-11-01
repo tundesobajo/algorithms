ab570fc17e5dbf97c1910fc7c25ec6f31d818934
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-11-01 23:19:10 +0100

    Edit sliding-window tests to use chai

--- src/sliding-window/consecutive-val-max-sum.spec.js
+++ src/sliding-window/consecutive-val-max-sum.spec.js
@@ -1,26 +1,26 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const consecutiveValMaxSum = require('./consecutive-val-max-sum');
 
 describe('Consecutive Value Max Sum', function () {
   it('returns 10 as max consecutive sum', function () {
     const arr = [1, 2, 5, 2, 8, 1, 5];
-    assert.strictEqual(consecutiveValMaxSum(arr, 2), 10);
+    expect(consecutiveValMaxSum(arr, 2)).to.equal(10);
   });
-  
+
   it('returns 17 as max consecutive sum', function () {
     const arr = [1, 2, 5, 2, 8, 1, 5];
-    assert.strictEqual(consecutiveValMaxSum(arr, 4), 17);
+    expect(consecutiveValMaxSum(arr, 4)).to.equal(17);
   });
 
   it('returns 6 as max consecutive sum', function () {
-    assert.strictEqual(consecutiveValMaxSum([4, 2, 1, 6], 1), 6);
+    expect(consecutiveValMaxSum([4, 2, 1, 6], 1)).to.equal(6);
   });
 
   it('returns 13 as max consecutive sum', function () {
-    assert.strictEqual(consecutiveValMaxSum([4, 2, 1, 6, 2], 4), 13);
+    expect(consecutiveValMaxSum([4, 2, 1, 6, 2], 4)).to.equal(13);
   });
 
   it('returns null', function () {
-    assert.strictEqual(consecutiveValMaxSum([], 4), null);
+    expect(consecutiveValMaxSum([], 4)).to.be.undefined;
   });
 });

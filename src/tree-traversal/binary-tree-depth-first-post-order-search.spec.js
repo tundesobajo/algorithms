d52243ec10285c6f46cd8000503f6a825d0fc802
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-11-10 23:26:40 +0100

    Add binary tree depth first search algorithm
    A post-order search pattern version.

---
+++ src/tree-traversal/binary-tree-depth-first-post-order-search.spec.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const BinaryTree = require('../data-structure/binary-tree');
+const arrayToTree = require('../utils/array-to-tree');
+const binaryTreeDepthFirstPostOrderSearch = require('./binary-tree-depth-first-post-order-search');
+
+describe('Breath First Tree Search', function () {
+  it('returns []', function () {
+    const tree = new BinaryTree();
+    expect(binaryTreeDepthFirstPostOrderSearch(tree)).to.be.empty;
+  });
+
+  it('returns [5]', function () {
+    const tree = arrayToTree([5]);
+    const result = [5];
+    expect(binaryTreeDepthFirstPostOrderSearch(tree)).to.have.members(result);
+  });
+
+  it('returns [6, 7, 5]', function () {
+    const tree = arrayToTree([5, [6, 7]]);
+    const result = [6, 7, 5];
+    expect(binaryTreeDepthFirstPostOrderSearch(tree)).to.have.ordered.members(result);
+  });
+
+  it('returns [8, 9, 6, 10, 11, 7, 5]', function () {
+    const tree = arrayToTree([5, [[6, [8, 9]], [7, [10, 11]]]]);
+    const result = [8, 9, 6, 10, 11, 7, 5];
+    expect(binaryTreeDepthFirstPostOrderSearch(tree)).to.have.ordered.members(result);
+  });
+});

5917b3758de10d8f78b993cf199172dfa584e1b8
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2020-11-13 21:15:09 +0100

    Add second largest value in BST algorithm
    Binary Search Tree traversal example.

---
+++ src/tree-traversal/bst-second-largest-value.js
@@ -0,0 +1,60 @@
+const BinarySearchTree = require('../data-structure/binary-search-tree');
+const BinaryTreeNode = require('../data-structure/binary-tree-node');
+
+/**
+ * Find node with the largest value.
+ * This will always be the rightmost node.
+ * 
+ * @param {BinaryTreeNode} startNode Starting point of search
+ * 
+ * @returns {BinaryTreeNode} Node with largest value
+ */
+function largestNode(startNode) {
+  let node = startNode;
+  while (node.right) {
+    node = node.right;
+  }
+
+  return node;
+}
+
+/**
+ * Find the second largest value in a binary search tree.
+ *       +---(3)-----------+
+ *  +---(2)        +---(5)------------------+ 
+ * (1)            (4)             +--------(9)
+ *                           +---(6)---+
+ *                          (7)       (8)
+ * The right-most node in the example above is 9 making it the largest.
+ * If 9 has a left node, then the second largest which is 8 in this
+ * example is the largest node starting from the left of 9.
+ * If 9 does not have a left node then the second largest is the
+ * parent node of 9 which would be 5 in this example.
+ * 
+ * @param {BinarySearchTree} tree Tree to search
+ * 
+ * @returns {any} Largest value
+ */
+module.exports = function binarySearchTreeSecondLargestValue(tree) {
+  const { left, right } = tree.root;
+  if (!tree.root || !(left || right)) {
+    throw new Error('Tree must have at least 2 nodes');
+  }
+
+  let parentOfLargest;
+  let largest = tree.root;
+  while (largest.right) {
+    parentOfLargest = largest;
+    largest = largest.right;
+  }
+
+  if (largest.left) {
+    // If the node with the largest value has a left subtree,
+    // then the second largest is in that left subtree.
+    return largestNode(largest.left).value;
+  } else {
+    // If the node with the largest value does not have a left subtree
+    // then it's parent node is the second largest.
+    return parentOfLargest.value;
+  }
+}
---
+++ src/tree-traversal/bst-second-largest-value.spec.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const BinarySearchTree = require('../data-structure/binary-search-tree');
+const binarySearchTreeSecondLargestValue = require('./bst-second-largest-value');
+
+describe('Binary Search Tree Second Largest Value', function () {
+  describe('#binarySearchTreeSecondLargestValue', function () {
+    it('returns 3 for [7, 3]', function () {
+      const tree = new BinarySearchTree([7, 3]);
+      expect(binarySearchTreeSecondLargestValue(tree)).to.equal(3);
+    });
+
+    it('returns 3 for [3, 7]', function () {
+      const tree = new BinarySearchTree([3, 7]);
+      expect(binarySearchTreeSecondLargestValue(tree)).to.equal(3);
+    });
+
+    it('returns 6 for [3, 7, 4, 6]', function () {
+      const tree = new BinarySearchTree([3, 7, 4, 6]);
+      expect(binarySearchTreeSecondLargestValue(tree)).to.equal(6);
+    });
+  });
+});
